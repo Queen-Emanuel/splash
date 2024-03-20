63cd90bf6c6ba3b36bc5533ddf2b3e6cd9338241
Queen-Emanuel <Emanuel9260926@gmail.com>
2024-03-20 18:12:20 +0200

    open Activity from splash

--- app/src/main/java/com/example/splashhh/MainActivity.java
+++ app/src/main/java/com/example/splashhh/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.splashhh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivities(new Intent[]{intent});
+                finish();
+            }
+        }, 3000);
     }
 }
